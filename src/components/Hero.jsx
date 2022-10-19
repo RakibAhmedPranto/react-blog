6b103b75b09a2c7789fe639bb3fa9ca0567d3657
RakibAhmedPranto <rakib820@gmail.com>
2022-10-19 23:42:11 +0600

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {close,logo,menu} from '../assets';
+import styles from '../style';
+
+const Hero = () => {
+  return (
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+          
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
